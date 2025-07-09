4554064a974c119106d85e930ef64c3e44d10f39
Chirra Bobby <bobby@example.com>
2025-07-10 01:54:22 +0530

    adsense ad script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
       <head>
         {/* adsense meta code */}
         <meta name="google-adsense-account" content="ca-pub-1673399772769188"></meta>
+        {/* adsense add code script */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1673399772769188" crossorigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
