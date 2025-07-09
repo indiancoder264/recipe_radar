04d7bf4b59b92b6e4beceafd24060be1295ccc64
Chirra Bobby <bobby@example.com>
2025-07-10 01:05:25 +0530

    about page,privacy policy,contact,terms of service pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* adsense meta code */}
         <meta name="google-adsense-account" content="ca-pub-1673399772769188"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
